9680357e37d09d170f83e51730366dfc20d96995
SpaGettys <jgettys@gmail.com>
2018-02-12 01:53:26 -0800

    Setup Todo list configured with datasource and delegate methods.

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,65 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by SpaGettys on 2018/02/12.
+//  Copyright © 2018 spagettys. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController {
+    
+    let itemArray = ["Find Mikeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeee eeeeeeeeeeeeeeeeeeeeeeeeee", "Buy Burritos", "Play Monhan"]
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    //MARK - Tableview Datasourcde Methods
+    
+    // Note: Since we are sub-classing a UITableView Controller, after we set our delegate functions below, there is NO need to specify a delegate or datasoure withing this class, as this will happen for us behind the scenes.
+    // Also notice how we didn't need to create an IBOutlet for a TableView or anything like that either.
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell
+        
+    }
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        //print(itemArray[indexPath.row])
+        
+        // add/remove a checkmark accesory to the row.
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        }
+        else {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        
+        // Go back to a while color after tapping and "grayifiying" the cell's color.   
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
+    
+    
+    
+
+
+}
+
